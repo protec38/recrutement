451fb1ac4409e0db84b62cef2a760106b17330af
BuildTools <unconfigured@null.spigotmc.org>
2022-11-07 00:57:19 +0100

    WIP : improved questions support

--- candidates/forms.py
+++ candidates/forms.py
@@ -4,3 +4,4 @@ from django.forms import TextInput, CharField, inlineformset_factory
 from candidates.models import Candidate, Answer, Question, Diploma
 
 DiplomaFormset = inlineformset_factory(Candidate, Diploma, fields=['type', 'date', 'diploma_file', 'continuous_training_date'])
+QuestionsFormset = inlineformset_factory(Candidate, Answer, fields=['question', 'text'], extra=0)
